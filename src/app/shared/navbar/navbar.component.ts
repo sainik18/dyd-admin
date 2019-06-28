9b192cc636775e51104c3aa7d9044f04e113ce9a
Sai Kiran <sainik18@gmail.com>
2019-06-28 08:36:05 +0530

    Working on new devotion

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: "app-navbar",
@@ -6,11 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent {
+
+  constructor(private router : Router){
+
+  }
   toggleClass = "ft-maximize";
   public isCollapsed = true;
   ToggleClass() {
     if (this.toggleClass === "ft-maximize") {
       this.toggleClass = "ft-minimize";
     } else this.toggleClass = "ft-maximize";
+  }
+
+  ChangeLanguage(lang){
+    localStorage.setItem('lang',lang);
+    let re = /devotion/gi;
+    if(this.router.url.search(re) == 1){
+      location.reload()
+    }
   }
 }
