4c6643e37c7087cef9414fcdf38fbb6c11f74c6b
Sai Kiran <sainik18@gmail.com>
2019-07-18 22:15:38 +0530

    sidebar icons added

--- src/app/shared/sidebar/sidebar-routes.config.ts
+++ src/app/shared/sidebar/sidebar-routes.config.ts
@@ -3,22 +3,22 @@ import { RouteInfo } from './sidebar.metadata';
 export const ROUTES: RouteInfo[] = [
 
     {
-        path: '/dashboard', title: 'dashboard', icon: 'icon-book-open', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
+        path: '/dashboard', title: 'dashboard', icon: 'icon-home', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
     },
     {
-        path: '/newdevotion', title: 'New Devotion', icon: 'icon-book-open', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
+        path: '/newdevotion', title: 'New Devotion', icon: 'icon-note', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
     },
     {
-        path: '/devotion', title: 'Devotion', icon: 'icon-book-open', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
+        path: '/devotion', title: 'Devotion', icon: 'icon-notebook', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
     },
     {
-        path: '/users', title: 'Users', icon: 'icon-book-open', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
+        path: '/users', title: 'Users', icon: 'icon-user', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
     },
     {
-        path: '/activity', title: 'Activity', icon: 'icon-book-open', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
+        path: '/activity', title: 'Activity', icon: 'icon-bar-chart', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
     },
     {
-        path: '/settings', title: 'Settings', icon: 'icon-book-open', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
+        path: '/settings', title: 'Settings', icon: 'icon-settings', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
     },
     // {
     //     path: '', title: 'Menu Levels', icon: 'icon-layers', class: 'has-sub', badge: '1', badgeClass: 'badge badge-pill badge-danger float-right mr-3 mt-1', isExternalLink: false,
