8e42f3a571a968add2a097343bb406ff54059a2f
sainik18 <sainik18@gmail.com>
2019-12-16 23:11:48 +0530

    fixes

--- src/app/helper/constants.ts
+++ src/app/helper/constants.ts
@@ -1,6 +1,6 @@
 export const APP_CONSTANTS = {
     login_admin: 'http://67.205.149.51:3000/adminLogin',
-    getDevotions: 'http://67.205.149.51:3000/getDevotions',
+    getDevotions: 'http://67.205.149.51:3000/getallDevotions',
     insertDevotions: 'http://67.205.149.51:3000/insertDevotions',
     getDevotionById: 'http://67.205.149.51:3000/getDevotionById',
     getDevotionByDate: 'http://67.205.149.51:3000/getDevotionByDate',
